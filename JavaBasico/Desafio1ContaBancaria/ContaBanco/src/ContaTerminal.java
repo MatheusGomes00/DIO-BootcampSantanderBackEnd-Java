ba93b321f651d402df73b1e980b0a243cc561ccf
MatheusGomes notebook <matheus.recin@outlook.com>
2024-05-03 12:11:28 -0300

    feat: add ContaTerminal scann function

--- JavaBasico/Desafio1ContaBancaria/ContaBanco/src/ContaTerminal.java
+++ JavaBasico/Desafio1ContaBancaria/ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // TODO: Conhecer e importar a classe Scanner;
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println("Digite o nome");
+        String nome = scanner.next();
+        System.out.println("Digite o numero da conta");
+        int numeroConta = scanner.nextInt();
+        System.out.println("Digite o Agencia");
+        String agencia = scanner.next();
+        System.out.println("Digite o Saldo");
+        float saldo = scanner.nextFloat();
+        scanner.close();
+        
+        System.out.println("Ola " + nome + ", obrigado por criar uma conta em nosso banco, sua agencia e " + agencia + 
+        ", conta " + numeroConta + " e seu saldo " + saldo + " ja esta disponivel para saque.");        
     }
 }
