ff24261910be78df6c990faecf72e65f6f34e4c6
MatheusGomes notebook <matheus.recin@outlook.com>
2024-05-06 14:43:35 -0300

    feat: explorando tratamento de exceções

---
+++ JavaBasico/ControleDeFluxo/src/ExemploExcecaoPersonalizada.java
@@ -0,0 +1,19 @@
+public class ExemploExcecaoPersonalizada {
+    public static void main(String[] args) {
+        try{
+            String cepFormatado = formatarCep("237650648");
+            System.out.println(cepFormatado);
+        } catch(CepInvalidoException e){
+            System.out.println("O cep não corresponde com as regras de negócio");
+        }
+    }
+
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8){
+            throw new CepInvalidoException();
+        }else{
+            //simulando um cep formatado
+            return "23.765-064";
+        }
+    }
+}
---
+++ JavaBasico/ControleDeFluxo/src/ExemploExecao2.java
@@ -0,0 +1,36 @@
+public class ExemploExecao2 {    
+    public static void main(String[] args) {
+        try{
+            int[] numeros = {1, 2, 3};
+            System.out.println(numeros[5]);
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Erro: " +  e.getMessage() + "\nÍndice fora dos limites do array.");
+        }
+
+        try{
+            int resultado = 10 / 0;
+            System.out.println(resultado);
+        }catch(ArithmeticException e){
+            System.out.println("Erro: Divisão por zero.");
+        }
+
+        try{
+            String texto = null;
+            System.out.println(texto.length());
+        }catch(NullPointerException e){
+            System.out.println("Erro: Tentativa de acesso a um objeto nulo.");
+        }
+
+        // exceção personalizada
+        try {
+            int valor = -5;
+            if(valor < 0){
+                throw new ValorNegativoException("Erro: Valor negativo não é permitido");
+            }
+        } catch (ValorNegativoException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+}
