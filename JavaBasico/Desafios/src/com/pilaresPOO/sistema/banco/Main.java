5b3e20618bc688709a412534393d84eecb83eb27
MatheusGomes notebook <matheus.recin@outlook.com>
2024-06-06 19:48:56 -0300

    feat/006/create-Main-class
    criada classe Main

---
+++ JavaBasico/Desafios/src/com/pilaresPOO/sistema/banco/Main.java
@@ -0,0 +1,31 @@
+package com.pilaresPOO.sistema.banco;
+
+public class Main {
+    public static void main(String[] args) {
+        Banco banco1 = new Banco();
+        Cliente cliente1 = new Cliente("Matheus", "123456");
+
+        Conta conta1 = ContaPoupanca.criarConta(cliente1);
+        Conta conta2 = ContaCorrente.criarConta(cliente1);
+
+        System.out.println(conta1.toString());
+
+        System.out.println(conta2.toString());
+
+
+        conta1.depositar(100);
+        conta1.exibirExtrato();
+
+        banco1.showContas();
+
+        conta1.transferir(50, conta2);
+
+        conta1.exibirExtrato();
+
+        conta2.exibirExtrato();
+
+        conta2.sacar(25);
+        conta2.exibirExtrato();
+
+    }
+}
