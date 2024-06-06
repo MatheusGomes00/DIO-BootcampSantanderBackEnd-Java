7233c3f70dc4c7146b0ee6a954c786e9f59685ff
MatheusGomes notebook <matheus.recin@outlook.com>
2024-06-06 19:48:03 -0300

    feate/005/create-ContaPoupanca-class
    criada classe ContaPoupanca

--- JavaBasico/Desafios/src/com/pilaresPOO/sistema/banco/ContaPoupanca.java
+++ JavaBasico/Desafios/src/com/pilaresPOO/sistema/banco/ContaPoupanca.java
@@ -1,14 +1,23 @@
 package com.pilaresPOO.sistema.banco;
 
 public class ContaPoupanca extends Conta{
+    // private static String limiteSaqueDiario
+    // private static String limiteTransferencia
+
     public ContaPoupanca(Cliente cliente) {
         super(cliente);
+    }
+
+    public static Conta criarConta(Cliente cliente){
+        Conta nova_conta = new ContaPoupanca(cliente);
+        Banco.addConta(nova_conta);
+        return nova_conta;
     }
 
     @Override
     public void exibirExtrato() {
         System.out.println("=== Extrato Conta Poupança ===");
-        super.exibirExtrato();
-        System.out.println("==============================");
+        super.imprimirMovimentacoes();
+        System.out.println("==============================\n");
     }
 }
