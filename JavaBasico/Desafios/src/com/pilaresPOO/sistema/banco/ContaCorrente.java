202869e25c52aee2930edcc332fb45583de85166
MatheusGomes notebook <matheus.recin@outlook.com>
2024-06-06 19:47:31 -0300

    feate/004/create-ContaCorrente-class
    criada classe ContaCorrente

--- JavaBasico/Desafios/src/com/pilaresPOO/sistema/banco/ContaCorrente.java
+++ JavaBasico/Desafios/src/com/pilaresPOO/sistema/banco/ContaCorrente.java
@@ -1,14 +1,24 @@
 package com.pilaresPOO.sistema.banco;
 
 public class ContaCorrente extends Conta{
+    // private static String limiteSaqueDiario
+    // private static String limiteTransferencia
+
+
     public ContaCorrente(Cliente cliente) {
         super(cliente);
+    }
+
+    public static Conta criarConta(Cliente cliente){
+        Conta nova_conta = new ContaCorrente(cliente);
+        Banco.addConta(nova_conta);
+        return nova_conta;
     }
 
     @Override
     public void exibirExtrato() {
         System.out.println("=== Extrato Conta Corrente ===");
-        super.exibirExtrato();
-        System.out.println("==============================");
+        super.imprimirMovimentacoes();
+        System.out.println("==============================\n");
     }
 }
