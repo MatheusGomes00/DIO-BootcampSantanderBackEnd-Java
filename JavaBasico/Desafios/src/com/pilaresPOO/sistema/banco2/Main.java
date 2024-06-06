fbdf29a247fc88e8e8bd222d4e704b992e2366af
MatheusGomes notebook <matheus.recin@outlook.com>
2024-06-06 19:50:19 -0300

    feat/006/create-MainTest-class
    criada classe Main como teste do modificador de acesso default na classe Banco

---
+++ JavaBasico/Desafios/src/com/pilaresPOO/sistema/banco2/Main.java
@@ -0,0 +1,15 @@
+package com.pilaresPOO.sistema.banco2;
+
+//import com.pilaresPOO.sistema.banco.Banco;
+
+public class Main {
+    public static void main(String[] args) {
+        //Banco banco1 = new Banco();
+        /*  vai apresentar erro na compilação
+            a classe banco foi declarada com o
+             modificador default e só pode ser
+             acessada de dentro do pacote
+             erro (build failed - cannot be accessed from outside package)
+        */
+    }
+}
